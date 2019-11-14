45629a37d0c197ac708f0e08c390c4a40108f503
phlinhlam <phlinh.lam@gmail.com>
2019-11-14 16:32:03 -0500

    completed

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static String pass = " ";
     public static void main(String[] args) {
         //populate Admin account
-        Admin admin = new Admin("Ad-001","Leeo Bee","admin1@email.com","password");
+        Admin admin = new Admin("Ad-001","Victor","admin1@email.com","password");
         admins.add(admin);
         //populate classes,students,and faculties
         populateDatabase();
@@ -21,7 +21,6 @@ public class Main {
         String yesNo = " ";
         String choose = " ";
 
-        boolean correct = true;
         System.out.println("Welcome to Bee's School System!");
         System.out.println("Would you like to log in? (Y/N)");
         yesNo = kb.next();
@@ -36,7 +35,7 @@ public class Main {
             choose = kb.next();
         }
             if(choose.equalsIgnoreCase("a")) {
-                System.out.println("Welcome admin ");
+                System.out.println("Welcome Admin");
                 System.out.println("Please enter your email:");
                 em = kb.next();
                 System.out.println("Please enter your password");
@@ -60,33 +59,41 @@ public class Main {
                             int choice = kb.nextInt();
                             switch (choice) {
                                 case 1:
+                                    System.out.println("Adding Student");
                                     addStudent();
                                     showStudent();
                                     break;
                                 case 2:
+                                    System.out.println("Adding Faculty");
                                     addFaculty();
                                     showFaculty();
                                     break;
                                 case 3:
+                                    System.out.println("Editing Student Information");
                                     editStudent();
                                     showStudent();
                                     break;
                                 case 4:
+                                    System.out.println("Editing Faculty Information");
                                     editFaculty();
                                     showFaculty();
                                     break;
                                 case 5:
+                                    System.out.println("Adding classes");
                                     addClasses();
                                     showClasses();
                                     break;
                                 case 6:
+                                    System.out.println("Editing Classes Description");
                                     editClasses();
                                     showClasses();
                                     break;
                                 case 7:
+                                    System.out.println("Enrolling student in a class");
                                     enrollStudent();
                                     break;
                                 case 8:
+                                    System.out.println("Assigning faculty to a class");
                                     assignFacultyToClass();
                                     break;
                                 case 9:
@@ -99,15 +106,18 @@ public class Main {
                                     System.exit(0);
                                     break;
                             }//end switch
+
                             System.out.println("Do you want to continue with other options? (Y/N)");
                             yesNo = kb.next();
                             System.out.println("");
                         } while (yesNo.equalsIgnoreCase("y"));
+
                         System.out.println(a.getName() + ", you have successfully logged out");
                         System.out.println("Thank you for using Bee's School Systems");
+
                     }//end if log in is correct
-                }//end for
-            }//end if user choose a
+                }//end for to loop through admin database
+            }//end if user choose 'a'
     }//end main
 
     public static void populateDatabase()
@@ -134,8 +144,8 @@ public class Main {
         classes.add(c1);
         Class c2 = new Class("C-002","Data Analytics","Collecting Data and stuff");
         classes.add(c2);
+    }//end populate database method
 
-    }
     public static void enrollStudent(){
         StudentClassConnection studClassC = new StudentClassConnection();
         Scanner kb = new Scanner(System.in);
@@ -166,44 +176,8 @@ public class Main {
         String classID = kb.nextLine();
         fcc.setFacClassConID(classID);
         facultyClassConnections.add(fcc);
-    }
-    public static void editFaculty() {
-        Scanner kb = new Scanner(System.in);
-        System.out.println("Please enter the ID of the Faculty that you wish to edit their name (F-###)");
-        String id = kb.nextLine();
-        for (Faculty f : faculties) {//go into faculties database,
-            if (f.getId().equalsIgnoreCase(id)) {//compare is with user id, if it's equal, do the execution
-                System.out.println("Enter new name of Faculty:");
-                f.setName(kb.nextLine());
-                break;
-            }
-        }//end for loop
-    }
-    public static void editStudent() {
-        Scanner kb = new Scanner(System.in);
-        System.out.println("Please enter the ID of the Student that you wish to edit their name(S-###)");
-        String id = kb.nextLine();
-        for (Student s : students) {//go into students database,
-            if (s.getId().equalsIgnoreCase(id)) {//compare is with user id, if it's equal, do the execution
-                System.out.println("Enter new name of student:");
-                s.setName(kb.nextLine());
-                break;
-            }
-        }//end for loop
-    }
-    public static void editClasses()
-    {
-        Scanner kb = new Scanner(System.in);
-        System.out.println("Please enter the ID of the class that you wish to edit its description (C-###)");
-        String id = kb.nextLine();
-        for(Class c : classes) {//go into classes database,
-                if(c.getClassID().equalsIgnoreCase(id)) {//compare is with user id, if it's equal, do the execution
-                        System.out.println("Enter new description for " +c.getClassName());
-                        c.setDescription(kb.nextLine());
-                        break;
-                    }
-        }//end for loop
-    }
+    }//end assigning faculty method
+
     public static void addClasses()
     {
         Class c = new Class();
@@ -218,7 +192,8 @@ public class Main {
         String desc = kb.nextLine();
         c.setDescription(desc);
         classes.add(c);//add class to database
-    }
+    }//end add class method
+
         public static void addFaculty()
         {
             Faculty f = new Faculty();
@@ -230,7 +205,8 @@ public class Main {
             String name = kb.nextLine();
             f.setName(name);
             faculties.add(f);//add faculty into faculties database
-        }
+        }//end add faculty method
+
         public static void addStudent()
         {
                 Student s = new Student();
@@ -242,7 +218,47 @@ public class Main {
                 String name = kb.nextLine();
                 s.setName(name);
                 students.add(s);//add student info in students database
-            }
+            }//end add student method
+
+        public static void editFaculty() {
+            Scanner kb = new Scanner(System.in);
+            System.out.println("Please enter the ID of the Faculty that you wish to edit their name (F-###)");
+            String id = kb.nextLine();
+            for (Faculty f : faculties) {//go into faculties database,
+                if (f.getId().equalsIgnoreCase(id)) {//compare is with user id, if it's equal, do the execution
+                    System.out.println("Enter new name of Faculty:");
+                    f.setName(kb.nextLine());
+                    break;
+                }
+            }//end for loop
+        }//end edit faculty method
+
+        public static void editStudent() {
+            Scanner kb = new Scanner(System.in);
+            System.out.println("Please enter the ID of the Student that you wish to edit their name(S-###)");
+            String id = kb.nextLine();
+            for (Student s : students) {//go into students database,
+                if (s.getId().equalsIgnoreCase(id)) {//compare is with user id, if it's equal, do the execution
+                    System.out.println("Enter new name of student:");
+                    s.setName(kb.nextLine());
+                    break;
+                }
+            }//end for loop
+        }//end edit student method
+
+        public static void editClasses()
+        {
+            Scanner kb = new Scanner(System.in);
+            System.out.println("Please enter the ID of the class that you wish to edit its description (C-###)");
+            String id = kb.nextLine();
+            for(Class c : classes) {//go into classes database,
+                if(c.getClassID().equalsIgnoreCase(id)) {//compare is with user id, if it's equal, do the execution
+                    System.out.println("Enter new description for " +c.getClassName());
+                    c.setDescription(kb.nextLine());
+                    break;
+                }
+            }//end for loop
+        }//end edit student method
 
             public static void showStudent()
             {
@@ -251,7 +267,7 @@ public class Main {
                 for (Student s : students) {
                     System.out.println(s.toString());
                 }
-            }
+            }//end show student method
 
             public static void showFaculty()
             {
@@ -260,7 +276,8 @@ public class Main {
                 for (Faculty f : faculties) {
                     System.out.println(f.toString());
                 }
-            }
+            }//end show faculty method
+
             public static void showClasses()
             {
                 System.out.println("---------------------------------");
@@ -268,7 +285,7 @@ public class Main {
                 for (Class c : classes) {
                     System.out.println(c.toString());
                 }
-            }
+            }//end show classes method
 
             public static void showInformation() {
                 System.out.println("---------------------------------");
@@ -286,6 +303,7 @@ public class Main {
                 for (Class c : classes) {
                     System.out.println(c.toString());
                 }
+
                 System.out.println("---------------------------------");
                 System.out.println("CLASSES TAUGHT BY FACULTIES");
                 for (FacultyClassConnection fcc : facultyClassConnections) {
@@ -303,7 +321,8 @@ public class Main {
                         }
                     }
                     System.out.println(" ");
-                }//end big for
+                }//end big for faculty-class
+
                 System.out.println("---------------------------------");
                 System.out.println("STUDENT ENROLLMENT");
                 for (StudentClassConnection scc : studentClassConnections) {
@@ -321,8 +340,7 @@ public class Main {
                     }
                     System.out.println("Date enrolled: "+scc.getDateEnroll());
                     System.out.println(" ");
-                }//end big for
+                }//end big for student-class
             }//end showInfo method
-
 
 }
